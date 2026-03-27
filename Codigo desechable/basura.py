f2da43c9b50ae32e6a80912206eae48c35037805
johann-alt-16 <johajesus14@gmail.com>
2026-03-27 14:49:11 -0400

    maraton

--- Codigo desechable/basura.py
+++ Codigo desechable/basura.py
@@ -1,10 +1,25 @@
 #Probando codigo
-def colores(eleccion_de_usuario=input('que color quieres maestro?: ').lower()):
-     colores_disponibles= ['negro', 'azul', 'verde']
-     print(  )
-   
 
-     if eleccion_de_usuario in colores_disponibles:
+def colores( ):
+   eleccion_de_usuario=input('que color quieres maestro?: ').lower()
+   colores_disponibles= ['negro', 'azul', 'verde']
+
+    
+    
+
+   if eleccion_de_usuario in colores_disponibles:
       print(f"¡ufff que buen gusto compa! ese {eleccion_de_usuario} te quedara guapisimoo")
-      
+
+   else:
+        print(f"Lo siento wey el color {eleccion_de_usuario} no lo tenemos disponible")
+
+
+def pantalones ():
+ Usuario= input(" Que pantalon quieres rey?: ").capitalize()
+ Pantalones_Disponible=["De vestir", "Clasico", "Cargo"]
+ 
+ if Usuario in Pantalones_Disponible:
+    print(f"Ohhhh con un pantalon {Usuario} vas a ser el centro de atencion")
+
 colores()
+pantalones()
