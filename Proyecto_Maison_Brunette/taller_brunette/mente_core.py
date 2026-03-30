10358c5b1f7831318614ef765753080bb6d5c175
johann-alt-16 <johajesus14@gmail.com>
2026-03-30 13:32:14 -0400

    Logre

--- Proyecto_Maison_Brunette/taller_brunette/mente_core.py
+++ Proyecto_Maison_Brunette/taller_brunette/mente_core.py
@@ -1,23 +1,54 @@
 #Se convoca las herramientas del corazon
 from corazon_core import ropa_caracteristica
 from corazon_core import ropa_caracteristica2
+#Se crea diccionario con el valor de luz que reflejan los colores
+VALOR_DE_BRILLO= {
+    "negro": 0,
+    "marino":2,
+    "cafe":3,
+    "gris":5,
+    "azul":6,
+    "verde":6,
+    "rojo": 7,
+    "naranja":8,
+    "amarillo":9,
+    "blanco": 10
+    }
+#Se le enseña que hacer con las herramientas
+def resultado_de_mezcla(color1, color2):
 
-#se le dan herramientas 
-ropa_caracteristica("camisa", "azul", 4)
-ropa_caracteristica2("pantalon", "rojo", 11)
+    recetario_de_colores ={
+        ("azul" , "amarillo"): "verde",
+        ("rojo", "azul"): "morado",
+        ("rojo", "amarillo"): "naranja",
+        ("blanco", "negro"): "gris",
+        ("blanco", "rojo"): "rosado"
+    }
+#Es util para detectar que "azul , amarillo" es igual a "amarillo , azul"
+    resultado= recetario_de_colores.get((color1 , color2)) or recetario_de_colores.get((color2 , color1))
 
-# se enseña que hacer con las herramientas
-# Recuerda que se utilizan las llaves del diccionario (Keys)
-def juzgar_ropa(mod1, mod2):
-    suma= mod1["intensidad"] + mod2["intensidad"]
-    if suma <= 10:
-        return "Perfectos colores mi amor, este ouffit te quera super bien"
-    else: 
-        return "Busquemos otros colores, te parece?"
-    
-ropa1= ropa_caracteristica("camisa", "azul", 4)
-ropa2= ropa_caracteristica2("pantalon", "negro", 0)
+#Solo comprimi el if/else para no ocupar mas espacio, no cambia absolutamente nada.
+    return resultado if resultado else "combinacion libre"
 
-resultado= juzgar_ropa(ropa1 , ropa2)
+def genera_veredicto_final(prenda_superior, prenda_inferior):
+    prenda_superior = ("color").lower()
+    prenda_inferior= ("color").lower()
 
+    luz_total= VALOR_DE_BRILLO.get(prenda_superior , 5 ) + VALOR_DE_BRILLO.get(prenda_inferior , 5)
+    mezcla= resultado_de_mezcla(prenda_superior , prenda_inferior)
+
+    if luz_total >= 16:
+        mensaje="¡MUYYY BRILLANTE , estas proyectando muchos colores"
+    elif luz_total<= 4:
+        mensaje= "Elegancia profunda..."
+
+    else:
+        mensaje= "Equilibrio maestro"
+
+    return f"Resultado visual: {mezcla} | Energia: {luz_total} /20\nConsejo:{mensaje}"
+
+prenda_superior = {"nombre": "camisa" , "color": "negro"}
+prenda_inferior = {"nombre": "pantalon" , "color": "rojo"}
+
+resultado=genera_veredicto_final(prenda_superior , prenda_inferior)
 print(resultado)
