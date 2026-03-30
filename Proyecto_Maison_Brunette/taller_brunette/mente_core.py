82238d29f4673176219d283230f19e30550f89ae
johann-alt-16 <johajesus14@gmail.com>
2026-03-30 13:52:53 -0400

    SIIIII

--- Proyecto_Maison_Brunette/taller_brunette/mente_core.py
+++ Proyecto_Maison_Brunette/taller_brunette/mente_core.py
@@ -25,17 +25,17 @@ def resultado_de_mezcla(color1, color2):
         ("blanco", "rojo"): "rosado"
     }
 #Es util para detectar que "azul , amarillo" es igual a "amarillo , azul"
-    resultado= recetario_de_colores.get((color1 , color2)) or recetario_de_colores.get((color2 , color1))
+    resultado= recetario_de_colores.get(color1 , color2) or recetario_de_colores.get((color2 , color1))
 
 #Solo comprimi el if/else para no ocupar mas espacio, no cambia absolutamente nada.
-    return resultado if resultado else "combinacion libre"
+    return resultado  if resultado else "combinacion libre"
 
 def genera_veredicto_final(prenda_superior, prenda_inferior):
-    prenda_superior = ("color").lower()
-    prenda_inferior= ("color").lower()
+    color_sup= prenda_superior ["color"].lower()
+    color_inf= prenda_inferior ["color"].lower()
 
-    luz_total= VALOR_DE_BRILLO.get(prenda_superior , 5 ) + VALOR_DE_BRILLO.get(prenda_inferior , 5)
-    mezcla= resultado_de_mezcla(prenda_superior , prenda_inferior)
+    luz_total= VALOR_DE_BRILLO.get(color_sup , 5 ) + VALOR_DE_BRILLO.get(color_inf , 5)
+    mezcla= resultado_de_mezcla(color_sup , color_inf)
 
     if luz_total >= 16:
         mensaje="¡MUYYY BRILLANTE , estas proyectando muchos colores"
@@ -47,8 +47,8 @@ def genera_veredicto_final(prenda_superior, prenda_inferior):
 
     return f"Resultado visual: {mezcla} | Energia: {luz_total} /20\nConsejo:{mensaje}"
 
-prenda_superior = {"nombre": "camisa" , "color": "negro"}
-prenda_inferior = {"nombre": "pantalon" , "color": "rojo"}
+prenda_superior = {"nombre": "camisa" , "color": "blanco"}
+prenda_inferior = {"nombre": "pantalon" , "color": "amarillo"}
 
 resultado=genera_veredicto_final(prenda_superior , prenda_inferior)
 print(resultado)
