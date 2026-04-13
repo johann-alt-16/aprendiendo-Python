11fa538b18f2001a6949e336c73d3fb34a78de87
johann-alt-16 <johajesus14@gmail.com>
2026-04-13 13:07:24 -0400

    3D finalizado

--- Proyecto_Maison_Brunette/taller_brunette/mente_logica.py
+++ Proyecto_Maison_Brunette/taller_brunette/mente_logica.py
@@ -1,10 +1,13 @@
+import math
 #Constante del cuerpo
 constantes={ 
   #Constantes promedio en hombres
   "ancho_de_hombros_H": 0.228, 
-  "ancho_cara_H": 0.088,
   "ancho_de_cadera_H": 0.191,
+  "ancho_cara_H": 0.088,
   "ancho_cintura_H": 0.145,
+  "ancho_pecho_H": 0.205,
+  "altura_pecho_H": 0.720,
   "altura_de_ojos_H": 0.936,
   "altura_del_hombro_H": 0.818,
   "altura_del_codo_H" : 0.630,
@@ -13,10 +16,12 @@ constantes={
   "altura_rodilla_H": 0.285,
   
   #Constantes promedio en mujeres
-  "ancho_de_hombros_M": 0.210,
+  "ancho_de_hombros_M": 0.212,
+  "ancho_de_cadera_M": 0.215,
   "ancho_cara_M": 0.085,
   "ancho_cintura_M": 0.138,
-  "ancho_de_cadera_M": 0.215,
+  "ancho_pecho_M": 0.198,
+  "altura_pecho_M": 0.705,
   "altura_de_ojos_M": 0.930,
   "altura_del_hombro_M": 0.810,
   "altura_del_codo_M" : 0.615,
@@ -28,29 +33,42 @@ constantes={
 usuario_silueta={
   "Reloj de arena":{
     "hombros": 1.25, #Ligero enfasis
+    "pecho": 1.20,#enfasis en busto
     "cintura": 0.85, #Cintura avispa
     "cadera": 1.25 #Curva suave
   },
   "Triangulo":{
     "hombros": 0.90, #Hombros estrechos
+    "pecho": 0.95,#torso superior mas bajo que el inferior
     "cintura": 1.10, #Cintura estandar (Cómun)
     "cadera": 1.45 #EL volumen mas notorio esta en esta zona
   },
   "Triangulo invertido":{
     "hombros": 1.45, #Hombros anchos
+    "pecho": 1.35, #PEcho ancho y estetico
     "cintura": 0.95, #CIntura que se reduce
     "cadera": 0.90 # Cadera muy estrecha
   },
   "Rectangulo":{
     "hombros": 1.15, #Todo alineado
+    "pecho": 1.10, #Alineacion perfcta
     "cintura": 1.12, #Poca definicion de cintura
     "cadera": 1.15 #Alineado con hombros
   },
   "Ovalado":{
     "hombros": 0.82, #Hombros redondeados
+    "pecho": 1.15, #volumen redondeado
     "cintura": 1.45, #Volumen maximo en la zona abdominal
     "cadera": 1.20 #Piernas suelen ser mas delgadas en proporcion a lo demas
   }
+}
+
+factores_sagitales={
+  "Reloj de arena": {"hombro":0.80, "pecho":0.85, "cintura":0.70, "cadera": 0.95},
+  "Triangulo":{"hombro":0.80,"pecho": 0.80, "cintura":0.75, "cadera": 1.05},
+  "Triangulo invertido":{"hombro":0.85,"pecho":1.10, "cintura":0.70, "cadera": 0.80},
+  "Rectangulo": {"hombro":0.80, "pecho":0.85, "cintura":0.80, "cadera":0.85},
+  "Ovalado":{"hombro":0.80,"pecho":0.95, "cintura":1.20, "cadera": 0.90}
 }
 def calcular_imc_y_estilo(peso,altura):
 
@@ -70,7 +88,7 @@ def calcular_imc_y_estilo(peso,altura):
 
  return factor_relleno
 
-def calcular_medida_osea(opcion, altura):
+def calcular_medida_osea(opcion):
    diccionario_de_medidas={
       "A": 0.92, # Estructura pequeña
       "B": 1.00, # Estructura media
@@ -78,15 +96,16 @@ def calcular_medida_osea(opcion, altura):
     }
    multiplicador= diccionario_de_medidas.get(opcion.upper(), 1.00)
    return multiplicador
-
-
-def calcular_anchura_del_cuerpo(altura, constantes, sexo):
+# PARTE TRANVERSAL (ANCHURA)
+def calcular_plano_transversal_del_cuerpo(altura, constantes, sexo):
 
   if sexo== "Masculino":
     ancho_de_hombros= altura * constantes['ancho_de_hombros_H']
     ancho_de_cadera= altura * constantes['ancho_de_cadera_H']
     ancho_cara= altura * constantes['ancho_cara_H']
     ancho_cintura= altura* constantes['ancho_cintura_H']
+    ancho_pecho= altura* constantes['ancho_pecho_H']
+    altura_pecho= altura* constantes['altura_pecho_H']
     altura_de_ojos= altura * constantes['altura_de_ojos_H']
     altura_del_hombro= altura * constantes['altura_del_hombro_H']
     altura_del_codo= altura* constantes['altura_del_codo_H']
@@ -100,46 +119,92 @@ def calcular_anchura_del_cuerpo(altura, constantes, sexo):
     ancho_cara= altura * constantes['ancho_cara_M']
     ancho_cintura= altura * constantes['ancho_cintura_M']
     print(ancho_cintura)
+    ancho_pecho= altura*constantes['ancho_pecho_M']
+    altura_pecho=altura*constantes['altura_pecho_M']
     altura_de_ojos= altura * constantes['altura_de_ojos_M']
     altura_del_hombro= altura * constantes['altura_del_hombro_M']
     altura_del_codo= altura* constantes['altura_del_codo_M']
     altura_de_cintura= altura* constantes['altura_de_cintura_M']
     altura_muñeca= altura * constantes['altura_muñeca_M']
     altura_rodilla= altura* constantes['altura_rodilla_M']
-  return ancho_de_hombros, ancho_de_cadera, ancho_cara, ancho_cintura, altura_de_ojos, altura_del_hombro, altura_del_codo, altura_de_cintura, altura_muñeca, altura_rodilla
-
-def calcular_anchura_final(altura, constantes,sexo,opcion_muneca, factor_relleno, usuario_silueta, tipo_cuerpo):
+  return ancho_de_hombros, ancho_de_cadera, ancho_cara, ancho_cintura,ancho_pecho,altura_pecho, altura_de_ojos, altura_del_hombro, altura_del_codo, altura_de_cintura, altura_muñeca, altura_rodilla
+def calcular_plano_transversal(altura, constantes,sexo,opcion_muneca, factor_relleno, usuario_silueta, tipo_cuerpo):
   ajuste= usuario_silueta[tipo_cuerpo]
-
   s="_M" if sexo == "Femenino"  else "_H"
 
   #Hombros
   hombros_final= (altura*constantes['ancho_de_hombros'+s])*opcion_muneca* factor_relleno *ajuste['hombros']
   print(hombros_final*100 )
+  pecho_final= (altura*constantes['ancho_pecho'+s])*opcion_muneca*factor_relleno*ajuste['pecho']
+  print(pecho_final*100)
   cintura_final= (altura*constantes['ancho_cintura'+s])*opcion_muneca* factor_relleno *ajuste['cintura']
   print(cintura_final*100 )
   cadera_final = (altura*constantes['ancho_de_cadera'+s])*opcion_muneca* factor_relleno *ajuste['cadera']
   print(cadera_final*100 )
+
+ 
+
   return hombros_final, cintura_final, cadera_final
+def eje_tranversal_X(hombros_final,pecho_final, cintura_final, cadera_final):
+ centro=0
+ radio_total_HOM= hombros_final/ 2
+  #EJE (X) POSITIVO(+)
+ derecha_hom= radio_total_HOM+ centro
+ #EJE NEGATIVO(-)
+ izquierda_hom=centro -radio_total_HOM
 
-def calculo_ICC(cintura_final, cadera_final):
- ICC = cintura_final/cadera_final
- print(ICC *100)
+ radio_total_PE= pecho_final/2
+ derecha_pe= radio_total_PE +centro
+ izquierda_pe=centro- radio_total_PE
 
-def calcular_proporcion_prenda(altura, estilo_elegido):
+ radio_total_CIN=cintura_final /2
+ derecha_cin= radio_total_CIN+centro
+ izquierda_cin= centro- radio_total_CIN
 
-  """ calcula el largo ideal de la prenda en centimetros"""
-  reglas_estilo={
-    "1": 0.38, 
-    "2": 0.40,
-    "3": 0.42,
-    "4": 0.45
-  }
+ radio_total_CA= cadera_final/2
+ derecha_ca=radio_total_CA+centro
+ izquierda_ca= centro-radio_total_CA
 
-  porcentaje= reglas_estilo.get(estilo_elegido, 0.40)
-  largo_cm= (altura *100) * porcentaje
-  
-  return round(largo_cm, 1)
-calcular_medida_osea("A", 1.80)
+ return (radio_total_HOM, derecha_hom, izquierda_hom), (radio_total_PE, derecha_pe, izquierda_pe), (radio_total_CIN, derecha_cin, izquierda_cin), (radio_total_CA, derecha_ca, izquierda_ca)
 
-calcular_anchura_final(1.80, constantes, "Femenino",1.02, 1.10, usuario_silueta, "triangulo")
+#PARTE SAGITAL(GROSOR)
+def calcular_plano_sagital(radio_total_HOM, radio_PE, radio_CIN, radio_CA, tipo_cuerpo):
+  factores= factores_sagitales[tipo_cuerpo]
+  z_radio_hom= radio_total_HOM*factores['hombros']
+  z_radio_pe= radio_PE * factores["pecho"]
+  z_radio_cin= radio_CIN* factores["cintura"]
+  z_radio_ca=radio_CA*factores["cadera"]
+
+
+  return z_radio_hom,z_radio_pe, z_radio_cin, z_radio_ca
+def calcular_eje_sagial_Z(z_radio_hom,z_radio_pe, z_radio_cin, z_radio_ca):
+  centro_Z=0
+#PARA HOMBROS
+  adelante_hom= centro_Z + z_radio_hom
+  atras_hom= centro_Z- z_radio_hom
+#PARA PECHO
+  adelante_pe= centro_Z + z_radio_pe
+  atras_pe= centro_Z-z_radio_pe
+#PARA CINTURA
+  adelante_cin= centro_Z+z_radio_cin
+  atras_cin= centro_Z-z_radio_cin
+#PARA CADERA
+  adelante_ca= centro_Z+z_radio_ca
+  atras_ca= centro_Z- z_radio_ca
+  return (adelante_hom, atras_hom),(adelante_pe,atras_pe), (adelante_cin,atras_cin), (adelante_ca, atras_ca)
+def calcular_elipse_contorno(radio_total_HOM, radio_total_PE, radio_total_CIN, radio_total_CA,z_radio_hom, z_radio_pe, z_radio_cin, z_radio_ca):
+  def ramanujan(a,b):
+    h= ((a-b)**2)/ ((a+b)**2)
+    perimetro= math.pi *(a+b)*(1+(3*h)/ (10 + math.sqrt(4-3 * h)))
+    return perimetro
+  perimetro_hombro= ramanujan(radio_total_HOM, z_radio_hom)
+  print(perimetro_hombro)
+  perimetro_pecho= ramanujan(radio_total_PE, z_radio_pe)
+  print(perimetro_pecho)
+  perimetro_cintura= ramanujan(radio_total_CIN, z_radio_cin)
+  print(perimetro_cintura)
+  perimetro_cadera=ramanujan(radio_total_CA, z_radio_ca)
+  print(perimetro_cadera)
+  return perimetro_hombro, perimetro_pecho, perimetro_cintura, perimetro_cadera
+
+calcular_plano_transversal(1.80, constantes, "Femenino", 1.00, 1.20, usuario_silueta, "Reloj de arena")
